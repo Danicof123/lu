43e755632e40f722c0d5c24b8f0102b19b9b676b
Danicof123 <danicoof@gmail.com>
2025-02-10 10:27:36 -0300

    rag

--- src/lu/retriever.ts
+++ src/lu/retriever.ts
@@ -1,4 +1,4 @@
-import { getEmbeddings } from "./llm/openai";
+import { getAIResponse, getEmbeddings } from "./llm/openai";
 
 interface retrieverProps {
 	revised_prompt: Content;
@@ -6,8 +6,14 @@ interface retrieverProps {
 		embedding: Array<number>;
 		[key: string]: any;
 	}>;
-	filter: string;
 	size?: number;
+}
+
+interface ragProps {
+	userData: UserData;
+	conversation: Messages;
+	chunks: Chunks;
+	model: Model;
 }
 
 //get the cosine similarity between two vectors
@@ -19,7 +25,7 @@ const cosineSimilarity = (vecA: Array<number>, vecB: Array<number>) => {
 }
 
 //get chunks with the highest cosine similarity to the revised prompt
-export const retriever = async ({ revised_prompt, data, size = 4 }: retrieverProps) => {
+export const retriever = async ({ revised_prompt, data, size = 4 }: retrieverProps): Promise<Chunks> => {
 	const embedding = await getEmbeddings(revised_prompt);
 
 	const chunks = data.map(item => ({
@@ -31,3 +37,25 @@ export const retriever = async ({ revised_prompt, data, size = 4 }: retrieverPro
 
 	return chunks.slice(0, size);
 }
+
+export const rag = async ({ chunks, conversation, userData, model }: ragProps) => {
+	const developerInstruction = `INFORMACIÓN A TENER EN CUENTA ANTES DE RESPONDER:
+- Información del usuario: ${JSON.stringify(userData)}
+- Base de conocimiento que se debe usar para responder: ${JSON.stringify(chunks)}
+
+REGLAS DE RESPUESTA:
+- Mismo idioma del usuario.
+- Estilo whatsapp, ejemplo para destacar palabra clave: *palabra*.
+- Emojis con coherencia.`;
+
+	const messages: Messages = [
+		{
+			role: "developer",
+			content: developerInstruction
+		},
+		...conversation
+	]
+
+	const AIresponse = await getAIResponse({ messages, model, temperature: 1 })
+	return AIresponse;
+};
--- src/types/luOpenai.d.ts
+++ src/types/luOpenai.d.ts
@@ -7,3 +7,10 @@ type Message = {
 type Messages = Message[];
 
 type Model = "gpt-4o-mini" | "text-embedding-3-small" | (string & {});
+type Chunk = {
+    embedding: Array<number>;
+    score: number;
+    [key: string]: any;
+}
+
+type Chunks = Chunk[];
