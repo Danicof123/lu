05e6cb0048db168a7b907e2ac283fc71f9c46294
Danicof123 <danicoof@gmail.com>
2025-02-26 16:39:41 -0300

    fix classifier; something output topic is JSON (stringify) other times only string. I added JSONParse

--- src/lu/classifier.ts
+++ src/lu/classifier.ts
@@ -1,4 +1,5 @@
 import { getAIResponse } from "./llm/openai";
+import { JSONparse } from "./parse";
 
 interface getTopicProps {
 	topics: Topics;
@@ -15,32 +16,46 @@ interface getRevisedPromptProps {
 
 export const getTopic = async ({ topics, conversation, model = "gpt-4o-mini", temperature = .5 }: getTopicProps) => {
 
-//get revised prompt
-const revisedPrompt = await getRevisedPrompt({ conversation, model, temperature });
+	//get revised prompt
+	const revisedPrompt = await getRevisedPrompt({ conversation, model, temperature });
 
-const developerInstruction = `Clasifica la entrada del usuario con las siguientes categorías de topics:
+	const developerInstruction = `Clasifica la entrada del usuario con las siguientes categorías de topics:
 \`${JSON.stringify(topics)}\`
 
 Pasos:
 - Analizar con atención a todos los topics'.
 - Seleccionar el topic que mejor se ajuste a la entrada del usuario.
-- Importante responder solo con el nombre del topic seleccionado sin agregar nada extra.`;
+- Importante responder SOLO con el nombre del topic seleccionado sin agregar nada extra.
+
+Formato respuesta:
+{
+	"topic": "nombre del topic"
+}`;
+
 
 	const messages: Messages = [
 		{ role: "user", content: revisedPrompt.content },
 		{ role: "developer", content: developerInstruction },
 	];
 
-	const topic = await getAIResponse({ messages, model, temperature });
+	const respTopic = await getAIResponse({ messages, model, temperature });
+	let topic = "ne";
+	try {
+		const parse = JSONparse(respTopic.content) as { topic: string, name: string };
+		topic = parse?.topic || parse?.name;
+	} catch (error) {
+		console.error(error);
+		topic = respTopic.content;
+	}
 
 	return {
-		price: topic.price + revisedPrompt.price,
+		price: respTopic.price + revisedPrompt.price,
 		revised_prompt: revisedPrompt.content,
-		topic: topic.content
+		topic
 	}
 }
 
-const getRevisedPrompt = async ({ conversation, model = "gpt-4o-mini", temperature=1, }: getRevisedPromptProps) => {
+const getRevisedPrompt = async ({ conversation, model = "gpt-4o-mini", temperature = 1, }: getRevisedPromptProps) => {
 	const developerInstruction = `ERES UN REFORMULADOR. A que se refiere el usuario con la última entrada? Reformula la entrada del usuario para que sea más clara y responde como si fuese el mismo usuario, es decir en primera persona. No agregues nada extra, solo responde con la reformulación.`;
 
 	// Create the message to be sent
